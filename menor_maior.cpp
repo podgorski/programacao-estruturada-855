dba31c94562265d14c769e3e50b14d7914b46513
ANTÔNIO BEZERRA <11074166710@UC.ACAD>
2018-03-15 08:18:05 -0300

    Added my first file

---
+++ menor_maior.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#define max 15
+
+int main() {
+	int v1[max];
+	int i, maior = NULL, menor = NULL;
+	for (i=0;i<max;i++){
+		printf("Digite um número :");
+		scanf("%d",&v1[i]);
+		if (i == 0){
+			maior = v1[i];
+			menor = maior;
+		}else{
+			if (v1[i] > maior){
+				maior = v1[i];
+			}else if (v1[i] < menor){
+				menor = v1[i];
+			}
+		}
+	}
+	printf("maior numero = %d, menor numero = %d\n",maior,menor);
+	
+	
+	return 0;
+}
